33be6fecba8ee308adfca086d4ac0b6aac6feae2
Vz-3 <vondc6201@gmail.com>
2024-08-09 15:01:09 +0800

    init refactor

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	semi: true, // Add semicolons
+	singleQuote: true, // Use single quotes
+	trailingComma: "es5", // Add trailing commas where applicable
+	printWidth: 80, // Set maximum line length
+	tabWidth: 2, // Use 2 spaces for indentation
+};
